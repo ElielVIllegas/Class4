a3764bc93b8002bac92471efb814a1ab6a77f4dc
Eliel Villegas <elielvillegas@Eliels-MacBook-Pro.local>
2017-09-08 16:10:45 -0500

    For and While Cycles

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+
+
+/*Programa para ciclos
+
+for es para cuando sabes cuantas veces vamos a iterar
+
+while no sabemos cuantas veces
+
+for(let I = 0; I < 5; I += 2) 
+
+{
+    //console.log("Hello, several times: " + I);
+
+}*/
+
+   /* for(let i = 1; i <= 4; i = i + 1)
+
+    repeat es para repetir un string
+
+    {
+        let line: string = "*"
+        line = line.repeat(i); 
+        console.log(line);
+    } */
+
+    let a = 1;
+
+    while(a<5)
+    {
+        let line: string = "*";
+        line = line.repeat(a);+
+        console.log(line);
+        a++;
+    }
+
+    
+
+
+
+
+
+
+
+
